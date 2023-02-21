ddee6f32f9ee85802132b1bcdd83ffd863b644be
Emma Feustel <erfeustel@gmail.com>
2023-02-21 07:31:06 -0500

    modified pit room that has dialogue dialogue is "vomited" ie not line by line unsure how to make sure player position is tracked

--- src/PitRoom.java
+++ src/PitRoom.java
@@ -25,5 +25,46 @@ public class PitRoom extends Room {
     public int[] getPitPosition() {
         return pitPosition;
     }
+    public class Main {
+    	public static void main(String[] args) {
+    		try {
+    			FileReader reader = new FileReader("trap.txt");
+    			//pukes entire file
+    			//working on line by line
+    			int data = reader.read();
+    			while(data != -1) {
+    				System.out.print((char)data);
+    				data = reader.read();
+    			}
+    			reader.close();
+    			if (player.position == pitPosition) {
+    				//unsure how player position will be tracked
+    				//so subject to change
+    				try {
+    	    			FileReader reader = new FileReader("pit.txt");
+    	    			//working on line by line
+    	    			int data = reader.read();
+    	    			while(data != -1) {
+    	    				System.out.print((char)data);
+    	    				data = reader.read();
+    	    			}
+    			} catch (FileNotFoundException e) {
+        			e.printStackTrace();
 
+        		} catch (IOException e) {
+        			e.printStackTrace();
+    			}
+    		}
+
+    		} catch (FileNotFoundException e) {
+    			e.printStackTrace();
+
+    		} catch (IOException e) {
+    			e.printStackTrace();
+
+    		}
+
+    	}
+
+    }
 }
