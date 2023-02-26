e66a73b185673994ae1a5aa9f4f2755bbf282a83
feustele <112406503+feustele@users.noreply.github.com>
2023-02-26 09:41:05 -0500

    Update and rename introRoom to introRoom.java
    Acts as an intro to the game, where players can choose to enter the cave or leave it, with intro/outro text accordingly.

---
+++ src/introRoom.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class introRoom extends Room {
+    public class Main {
+    	public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+    		try {
+    			FileReader reader = new FileReader("intro.txt");
+    			BufferedReader intro = new BufferedReader reader;
+                String line; 
+    			while((line = intro.readLine()) != null) {
+    				System.out.print(line);
+    			}
+                //reads out room enter text
+    			reader.close();
+          intro.close();
+    			System.out.println("Do you enter the cave?");
+		      String word1 = scnr.next();
+          if (word1 == "No" || word1 == "n" || word1 == "NO" || word1 == "no" || word1 == "N"){
+            //if the player chooses not to enter the cave, the game ends.
+    				try {
+    	    			FileReader chickenReader = new FileReader("chicken.txt");
+                BufferedReader chickenBuffer = new BufferedReader chickenReader;
+    			        while((line = chickenBuffer.readLine()) != null) {
+    				        System.out.print(line);
+    			        }
+                   chickenReader.close();
+                   chickenBuffer.close();
+             }
+                    // reads out fall text
+             catch (FileNotFoundException e) {
+        			  e.printStackTrace();
+        		 }
+             catch (IOException e) {
+        			  e.printStackTrace();
+    			   }
+    		}
+        else if (word1 == "Yes" || word1 == "Y" || word1 == "YES" || word1 == "yes" || word1 == "y"){
+          Room();
+          //if player enters the cave from the intro room, then a new room is generated.
+        }
+
+    		} catch (FileNotFoundException e) {
+    			e.printStackTrace();
+
+    		} catch (IOException e) {
+    			e.printStackTrace();
+
+    		}
+
+
+}
