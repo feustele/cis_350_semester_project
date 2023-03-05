2b8cd1c46b810d8dee30fbbea7a20f4682e430d4
Amanuelfufa <77419904+Amanuelfufa@users.noreply.github.com>
2023-03-05 10:17:22 -0500

    Update Monster.java
    this is an update.

--- src/Monster.java
+++ src/Monster.java
@@ -4,19 +4,33 @@ import java.util.ArrayList;
  * The parent class to all Monsters implemented. Holds the framework that will be used by all Monster.
  */
 public abstract class Monster {
-	//The amount of damage that a monster does
-    protected int combatPower = 0;
-    
+        /**
+	 * The amount of damage that a monster does
+	 */
+	private String name;
+	protected int combatPower = 0;
+	private int locX;
+	private int locY;
 	/**
 	 * Creates a new monster with the specified power level
+	 * @param name
 	 * @param combatPower
+	 * @param locX
+	 * @param locY
 	*/
-	public Monster (int combatPower) {
-		
+	public Monster (String name, int combatPower, int locX, int locY) {
+		this.name = name;
+		this.combatPower = combatPower;
+		this.locX = locX;
+		this.locY =locY;
 	}
-
-	
-	
+	/**
+	 * 
+	 * @return String
+	 */
+	public String getname() {
+		return name;
+	}
 	/** 
 	 * 
 	 * @return int
@@ -24,7 +38,20 @@ public abstract class Monster {
 	protected int getCombatPower() {
 		return combatPower;
 	}
-
+	/**
+	 * 
+	 * @return int
+	 */
+	public int getlocX() {
+		return locx;
+	}
+	/**
+	 * 
+	 * @return int
+	 */
+	public int getlocY() {
+		return locY;
+	}
 
 
 }
