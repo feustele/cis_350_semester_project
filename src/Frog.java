c30af5c0cc9e1718f8a48ba1d9e2d0fe223f99aa
Amanuelfufa <77419904+Amanuelfufa@users.noreply.github.com>
2023-03-05 10:19:56 -0500

    Update Frog.java
    This is Updated.

--- src/Frog.java
+++ src/Frog.java
@@ -2,8 +2,15 @@
  * A basic enemy that the player can encounter
  */
 public class Frog extends Monster{
-    public Frog(int combatPower) {
-        super(combatPower);
+	/**
+	 * 
+	 * @param name
+	 * @param combatPower
+	 * @param locX
+	 * @param locY
+	 */
+    public Frog(String name, int combatPower, int locX, int locY) {
+        super(name,combatPower,locX,locY);
     }
 
 }
