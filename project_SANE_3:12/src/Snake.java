997b663b247a247aa78c87a8517141b1471ac4b1
feustele <112406503+feustele@users.noreply.github.com>
2023-03-15 15:02:50 -0400

    Update Snake.java

--- project_SANE_3:12/src/Snake.java
+++ project_SANE_3:12/src/Snake.java
@@ -3,10 +3,12 @@ public class Snake extends Monster {
 	 * 
 	 * @param name
 	 * @param combatPower
-	 * @param locX
-	 * @param locY
 	 */
-	public Snake(String name, int combatPower, int locX, int locY) {
-		super(name,combatPower,locX,locY);
+	public Snake() {
+        	super("Snake",100); 
+    	}
+	
+	public Snake(String name, int combatPower) {
+		super(name,combatPower);
  }
 }
