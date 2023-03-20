a2ecd4f40aa05d6b2d0a0a7273f6afdb3abeb859
Amanuelfufa <77419904+Amanuelfufa@users.noreply.github.com>
2023-03-20 07:44:12 -0400

    Update Frog.java
    Java Doc Added.

--- src/Frog.java
+++ src/Frog.java
@@ -1,17 +1,25 @@
 /**
- * A basic enemy that the player can encounter
+ * A basic enemy that the player can encounter. This class extends the Monster class
+ * and adds functionality specific to the Frog enemy.
  */
 public class Frog extends Monster{
-	/**
-	 * 
-	 * @param name
-	 * @param combatPower
-	 */
-	public Frog() {
-        	super("Dart Frog",100); 
-    	}
-    public Frog(String name, int combatPower) {
-        super(name,combatPower);
-    }
 
+    /**
+     * Creates a new Frog with default properties.
+     * The default constructor calls the super constructor with default values.
+     */
+    public Frog() {
+        super("Dart Frog", 100); 
+    }
+    
+    /**
+     * Creates a new Frog with specified properties.
+     * @param name the name of the Frog enemy
+     * @param combatPower the combat power of the Frog enemy
+     */
+    public Frog(String name, int combatPower) {
+        super(name, combatPower);
+    }
 }
+
+
