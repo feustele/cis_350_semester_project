ffbc11a14ae00b75d54030f47ff7a34e2420d3fd
Amanuelfufa <77419904+Amanuelfufa@users.noreply.github.com>
2023-03-21 11:47:40 -0400

    Update MysteriousAmulet.java
    java doc updated

--- src/MysteriousAmulet.java
+++ src/MysteriousAmulet.java
@@ -1,10 +1,12 @@
 /**
  * An item that allows for the player to win the game
+ * Extends the QuestItem class.
  */
 public class MysteriousAmulet extends QuestItem {
 
     /**
      * Default Constructor creates a new MysteriousAmulet
+     * Calls the constructor of the parent QuestItem class.
      */
     public MysteriousAmulet() {
         super("DefaultEnding");
