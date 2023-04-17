3320fb786d4a54c1e885a5b951890f2ca310d1ae
TechtonicGuy <125809822+TechtonicGuy@users.noreply.github.com>
2023-04-16 20:18:08 -0400

    Update GUI.java

--- src/projectSane/GUI.java
+++ src/projectSane/GUI.java
@@ -1,40 +1,26 @@
 package projectSane;
+import java.awt.Color;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JTextPane;
 public class GUI {
+	JFrame window;
+	JTextPane text;
 	
 	public GUI() {
-		JFrame window = new JFrame();
-		JTextPane text = new JTextPane();
+		window = new JFrame();
+		text = new JTextPane();
 		window.setBounds(0, 0, 1000, 600);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		// Menu Bar
-//		JMenuBar menu = new JMenuBar();
-//		JMenu file = new JMenu("File");
-//		menu.add(file);
-//		JMenuItem save = new JMenuItem("Save");
-//		JMenuItem load = new JMenuItem("Load");
-//		JMenuItem quit = new JMenuItem("Quit");
-//		file.add(save);
-//		file.add(load);
-//		file.add(quit);
-//		window.add(menu);
-		
 		text.setEditable(false);
-		text.setText("Hello");
-		text.setText(text.getText() + "\n" + "No");
+		text.setBackground(Color.black);
 		
 		window.add(text);
 		window.setVisible(true);
 	}
 	
 	
-	
-	public static void main(String args[]) {
-		GUI gui = new GUI();
+	public void addText(String s) {
+		this.text.setText(this.text.getText() + "\n" + s);
 	}
 }
