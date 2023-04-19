4f45b5f01f915667640edb20f5c3d0253c0c483f
feustele <112406503+feustele@users.noreply.github.com>
2023-04-19 18:00:29 -0400

    Update outroLevel.java

--- src/projectSane/outroLevel.java
+++ src/projectSane/outroLevel.java
@@ -167,11 +167,13 @@ public class outroLevel extends Room {
 		char answer = promptUser("Do you retrieve the Mysterious Amulet?", gui);
 
 		if (answer == 'n') {
+			audioEngine.track.stop();
 			//if the player chooses not to take the amulet, the game ends.
 			audioEngine.playSong("thereaper.wav");
 			generateBadEnding(gui);
 	    			
 		}else if(answer == 'y') {
+			audioEngine.track.stop();
 			audioEngine.playSong("pleasestandup.wav");
 			generateGoodEnding(gui);	
 		}
