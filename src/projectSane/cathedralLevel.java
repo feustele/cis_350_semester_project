7335a76fb8d3599940b56521888a9417c41e7c9f
TechtonicGuy <125809822+TechtonicGuy@users.noreply.github.com>
2023-04-19 20:55:05 -0400

    Update cathedralLevel.java

--- src/projectSane/cathedralLevel.java
+++ src/projectSane/cathedralLevel.java
@@ -34,7 +34,7 @@ public class cathedralLevel extends Room {
 
 		do {
 			gui.addText(prompt);
-			input = scnr.next();
+			input = gui.getInput();
 		} while(!(
 			input.equalsIgnoreCase("No") || input.equalsIgnoreCase("N") 
 			|| input.equalsIgnoreCase("Yes") || input.equalsIgnoreCase("Y")));
@@ -113,7 +113,7 @@ public class cathedralLevel extends Room {
 				input.equalsIgnoreCase("n") || input.equalsIgnoreCase("w") 
 				|| input.equalsIgnoreCase("s") || input.equalsIgnoreCase("e"))){
 			gui.addText(prompt);
-			input = scnr.next();
+			input = gui.getInput();
 
 			if(!move(map, input, gui)) {
 				input = null;
